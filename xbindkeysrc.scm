62f77b23e9fa4719b5873425860e2ef0a7f41f5c
Zeno Zeng <zenoofzeng@gmail.com>
2014-04-24 02:28:41 +0800

    add kill/yank support

--- xbindkeysrc.scm
+++ xbindkeysrc.scm
@@ -4,6 +4,22 @@
   (run-command (string-append "xdotool getwindowfocus key --clearmodifiers " key))
   (grab-all-keys))
 
+(define (bind-key-sequence trigger key func)
+  ""
+  (xbindkey-function trigger
+                     (lambda ()
+                       (xbindkey-function
+                        key
+                        (lambda ()
+                          (func)
+                          ;; TODO: if that key exists in orignal map
+                          ;;       restore it.
+                          (remove-xbindkey key))))))
+
+(bind-key-sequence '(control x) '(k)
+                   (lambda ()
+                     (press "Control+w")))
+
 (xbindkey-function '(control n)
                    (lambda ()
                      (press "Down")))
@@ -31,3 +47,11 @@
 (xbindkey-function '(control s)
                    (lambda ()
                      (press "Control+f")))
+
+(xbindkey-function '(control w)
+                   (lambda ()
+                     (press "Control+x")))
+
+(xbindkey-function '(control y)
+                   (lambda ()
+                     (press "Control+v")))
